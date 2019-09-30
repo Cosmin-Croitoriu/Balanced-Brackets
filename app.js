021fabe0719fe423139f1ef0733b42317af0c4b4
Habsu <habucea@gmail.com>
2019-09-30 14:28:07 +0100

    Fail for single bracket

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const balancedBrackets = (string) => {
     if (!string || 0 === string.length){
         return "empty string"
-    }
+    }else if(string === "["){
+        return "FAIL";
+    };
 }
 module.exports = balancedBrackets;
--- app.test.js
+++ app.test.js
@@ -4,3 +4,6 @@ test ("if given an empty string it returns an empty string", () => {
     expect(balancedBrackets('')).toBe('empty string');
 });
 
+test ("returns FIL if string has 1 bracket", () => {
+    expect(balancedBrackets('[')).toBe('FAIL');
+});
